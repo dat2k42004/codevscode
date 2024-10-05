af6ed8dc25f9a98e393dbf60893cc669414e841d
dat2k42004 <124463330+nddat24@users.noreply.github.com>
2024-10-05 07:40:36 +0700

    update_6/10/2024

---
+++ java/practive/J05074.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class J05074 {
+
+    static int solve(String s) {
+        String[] res = s.split("\\s+");
+        int x = 10;
+        for (int i = 0; i < res[1].length(); ++ i ) {
+            if (res[1].charAt(i) == 'm')
+                x --;
+            else if (res[1].charAt(i) == 'v')
+                x -= 2;
+            if (x <= 0) 
+            {
+                return -1;
+            }
+        }
+        return x;
+    }
+
+    static class manager {
+        String id, name, clas;
+
+        manager(String x, String y, String z) {
+            id = x;
+            name = y;
+            clas = z;
+        }
+
+        @Override
+        public String toString() {
+            return id + " " + name + " " + clas + " ";
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc. nextLine();
+        
+        ArrayList<manager> a = new ArrayList<>();
+        for (int i = 0; i < n; ++ i)
+        {
+            a.add(new manager(sc.nextLine(), sc.nextLine(), sc.nextLine()));
+        }
+
+        String[] res = new String[n];
+
+        for (int i = 0; i < n; ++ i)
+        {
+            res[i] = sc.nextLine();
+        }
+
+        for (manager x : a) {
+            System.out.print(x);
+            for (int j = 0; j < n; ++ j) {
+                if (res[j].startsWith(x.id)) {
+                    if (solve(res[j]) == -1)
+                        System.out.println("0 KDDK");
+                    else 
+                        System.out.println(solve(res[j]));
+                }
+            }
+        }
+
+    }
+}
+
+
